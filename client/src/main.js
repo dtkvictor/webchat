b6d475ba2e9e438c4ff47201322410a01158a3b8
Victor Lucas <dtkvictor32@gmail.com>
2022-11-24 22:33:28 -0300

    Substituindo o materialize pelo bootstrap

--- client/src/main.js
+++ client/src/main.js
@@ -2,7 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import store from './store'
 import router from './router'
-import 'materialize-css/dist/js/materialize.min.js'
-import 'materialize-css/dist/css/materialize.min.css'
+
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'material-icons/iconfont/material-icons.css'
 
 createApp(App).use(store).use(router).mount('#app')
