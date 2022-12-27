d126b14bbbe568a73c5d52c3d500034d36dc191b
Victor Lucas <dtkvictor32@gmail.com>
2022-12-27 16:40:41 -0300

    Removendo ifs desnecessários

--- server/src/Model/Users.php
+++ server/src/Model/Users.php
@@ -14,11 +14,8 @@ class Users
     }  
 
     public function find(int $resourceId):object
-    {
-        if(isset($this->users[$resourceId])){
-            return $this->users[$resourceId];
-        }        
-        return new stdClass;
+    {        
+        return $this->users[$resourceId];        
     }     
 
     public function exists(int $resourceId):bool
@@ -29,7 +26,7 @@ class Users
     public function add(object $conn, int $resourceId, string $name, string $image = null):void
     {
         if(!isset($this->users[$resourceId])){            
-            
+
             $this->users[$resourceId] = new stdClass;
             $this->users[$resourceId]->send = $conn->send;            
             $this->users[$resourceId]->lastMessageTime = time();
@@ -43,25 +40,23 @@ class Users
         }
     }    
 
+    public function remove(int $resourceId):void
+    {        
+        unset($this->users[$resourceId]);       
+    }
+
     public function updateLastMessageTime(int $resourceId):void 
+    {        
+        $this->users[$resourceId]->lastMessageTime = time();        
+    }
+
+    public function incrementMessagesPerSecond(int $resourceId):void 
     {
-        if($this->exists($resourceId)) {
-            $this->users[$resourceId]->lastMessageTime = time();
-        }
+        $this->users[$resourceId]->messagesPerSecond ++;        
     }
 
     public function resetMessagesPerSecond(int $resourceId):void 
-    {
-        if($this->exists($resourceId)) {
-            $this->users[$resourceId]->messagesPerSecond = 0;
-        }
-    }
-
-    public function remove(int $resourceId):void
-    {
-        if($this->exists($resourceId)) {
-            unset($this->users[$resourceId]);
-        }        
-    }
-
+    {        
+        $this->users[$resourceId]->messagesPerSecond = 0;       
+    }    
 }
