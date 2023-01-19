4fd2bfcbe2f8947c1f8fbc5f86129205a041b70e
Victor Lucas <dtkvictor32@gmail.com>
2023-01-19 19:49:04 -0300

    Convertendo os ajudantes para uma class

--- client/src/support/helpers.js
+++ client/src/support/helpers.js
@@ -1,28 +1,45 @@
-export function removeHtmlTags(str) 
-{        
-    if(!str) return false;
-    return str.replace(/(<([^>]+)>)/ig, '')
-}
+export default class {
 
-export function renderByFormat(format, value) 
-{   
-    switch(format) {
-        case 'image':     
-            return `<img src=${value}>`                        
-        case 'video':
-            return `<video controls src=${value}></video>`                    
-        case 'audio':
-            return `<audio controls src=${value}></audio`                        
-        default:
-            return removeHtmlTags(value)
+    removeHtmlTags(str) 
+    {        
+        if(!str) return false;
+        return str.replace(/(<([^>]+)>)/ig, '')
     }
-}
 
-export function binaryToUrlBase64(binary) 
-{
-    const fileReader = new FileReader()                    
-    return new Promise(resolve => {        
-        fileReader.onload = (data) => resolve(data.target.result)                                                                     
-        fileReader.readAsDataURL(binary)        
-    })
+    renderByFormat(format, value) 
+    {   
+        switch(format) {
+            case 'image':     
+                return `<img src=${value}>`                        
+            case 'video':
+                return `<video controls src=${value}></video>`                    
+            case 'audio':
+                return `<audio controls src=${value}></audio`                        
+            default:
+                return value
+        }
+    }
+
+    binaryToUrlBase64(binary) 
+    {
+        const fileReader = new FileReader()                    
+        return new Promise(resolve => {        
+            fileReader.onload = (data) => resolve(data.target.result)                                                                     
+            fileReader.readAsDataURL(binary)        
+        })
+    }
+
+    defaultDataNotificationToats() 
+    {
+        return {
+            image: null,
+            title: null,                     
+            message: null,          
+            imageWidth: 100,
+            titleLineHeight: '25',                 
+            messageLineHeight: '25',                                                   
+            position: 'topCenter',  
+            timeout: 5000,        
+        }
+    }
 }
