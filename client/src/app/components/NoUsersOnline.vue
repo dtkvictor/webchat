4c4e38188f97dd917dee98fdbb720d88bd357bb7
Victor Lucas <dtkvictor32@gmail.com>
2023-01-24 13:31:09 -0300

    Criação de componente para notificar quando não tiver usuários online.

---
+++ client/src/app/components/NoUsersOnline.vue
@@ -0,0 +1,30 @@
+<template>
+    <modal :open="openModal" @close="close = true">
+        <template v-slot:body>
+            <div class="row align-items-center p-4 m-0 card-friend">                
+                <h1>:(</h1>
+                <h2>Poxa, parece que não tem ninguém online</h2>
+                <h3 class="fw-light">Que tal chamar alguns amigos???</h3>                                
+            </div>
+        </template>
+    </modal>
+</template>
+<script>
+import Modal from '@/app/components/Modal.vue'
+import { mapState } from 'vuex'
+export default {    
+    components: { Modal },  
+    data: () => ({
+        close: false
+    }),  
+    computed: {
+        ...mapState(['amountUsers']),
+        openModal(){
+            if(this.amountUsers < 1 && !this.close){
+                return true
+            }
+            return false
+        }
+    }
+}
+</script>
